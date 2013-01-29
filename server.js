395769afea8fc527cb50cf2638fc9d0e6f059801
velniukas <steve.g.messina@gmail.com>
2013-01-29 12:44:33 +0800

    added wordnet, pos support

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var natural = require('natural'),
+  pos = require('pos'),
+  wordnet = new natural.WordNet(),
+  _ = require('underscore'),
+  NGrams = natural.NGrams,
   classifier = new natural.BayesClassifier(),
   tokenizer = new natural.TreebankWordTokenizer(); // natural.WordTokenizer();
 
@@ -48,23 +52,54 @@ app.get('/qry', function (req, res) {
 
   var tokens;
   if (q) {
-  	  //res.write('query: '+q+'\n');
-	  //tokens = tokenizer.tokenize(q); 
-	  // or can do tokenize and stem
-	  tokens = q.tokenizeAndStem();
+  	var data = [];
+
+  	// we are going to search single and dual words for nouns to identify some semantics, e.g. names, cities etc
+  	var unigrams = NGrams.ngrams(q, 1);
+ 	var bigrams = NGrams.bigrams(q);
+ 	console.log('bigrams: '+bigrams);
+
+ 	var words = new pos.Lexer().lex(q);
+ 	var taggedWords = new pos.Tagger().tag(words);	  
+
+	//tokens = tokenizer.tokenize(q); 
+	// or can do tokenize and stem
+	tokens = q.tokenizeAndStem();
+
 	//  var category = classifier.getClassifications(req.params.qry);
-	  console.log(tokens);
-	//  console.log(category);
+	console.log(tokens);
 
-	//  res.send(category);
+	// identify things (nouns)
+	for (i in taggedWords) {
+		var taggedWord = taggedWords[i];
+		var word = taggedWord[0]; 
+		var tag = taggedWord[1];
+		// mark all nouns and verbs
+		if (_.contains(["NN", "NNP", "NNS", "NNPS", "URL", /*"VB", "VBD", "VBG", "VBN", "VBP", "VBZ"*/], tag)) {
+			console.log(word + " /" + tag);
+		} 
+	}
 
-	var data = [];
+	// lookup bigrams for Proper Nouns and significant words etc to combine into one 'meta phrase'
+	// if we find a lemma then this is a single entity (token), e.g. New York
+	for (i in bigrams) {
+		var phrase = bigrams[i].map(function(item) { return item.toString(); }).join(" ");
+		//console.log('phrase: '+phrase);
+		var lemma = wordnet.lookup( phrase, function( results ) {
+			results.some( function(result) {
+				console.log(phrase + ": " + result.pos + ", " + result.lemma + ", " + result.gloss);
+				data.push( {value: result.lemma} );
+				return result.lemma;
+			});
+		});
+	}
+
 	for (i in tokens) {
-		//var row = {id: i+"", label: tokens[i]+"", value: tokens[i]+""};
 		var row = {value: tokens[i]};
 		data.push(row);
 	}
 	console.log(data);
+
 	// JSON.parse(...) or data.toString() don't work
 	// only Stringify puts quotes around id, lavel, value which autocomplete expects
 	console.log(JSON.stringify(data));
